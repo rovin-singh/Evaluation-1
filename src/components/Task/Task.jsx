45aad226ad55cabdffe95e45fec58d4d229013e8
rovin-singh <rovin7678@gmail.com>
2022-05-23 16:59:57 +0530

    final

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./task.module.css";
 import Counter from "../Counter/Counter"
-const Task = ({dataArr}) => {
+  const Task = ({dataArr,completedData,comFun}) => {
   // NOTE: do not delete `data-cy` key value pair
   return (
     <div>
     {dataArr.map((data)=>(
         <li data-cy="task" className={styles.task}>
-        <input type="checkbox" data-cy="task-checkbox" />
+        <input type="checkbox" data-cy="task-checkbox" onClick={comFun}/>
         <div data-cy="task-text">
            <p>{data}</p>
         </div>
--- src/components/TaskApp.jsx
+++ src/components/TaskApp.jsx
@@ -10,16 +10,21 @@ const TaskApp = () => {
   const [dataArr,setData]=React.useState([])
   const [totalTask,settotalTask]=React.useState(0)
   const [unCompletedTask,setunCompletedTask]=React.useState(0)
+  const [completedData,setCompleteData]=React.useState(0)
+  
   const addTask=()=>{
     setData([...dataArr,task]);
     setTask("");
     settotalTask(dataArr.length+1);
   }
+  const comFun=(e)=>{
+    console.log(e)
+  }
   return (
     <div data-cy="task-app" className={styles.taskApp}>
       <TaskHeader totalTask={totalTask} unCompletedTask={unCompletedTask} />
       <AddTask  task={task} setTask={setTask} addTask={addTask} />
-      <Tasks  dataArr={dataArr} />
+      <Tasks  dataArr={dataArr} comFun={comFun} completedData={completedData} />
     </div>
   );
 };
